d2eae60f6473f940c87e82d366084b01f21c1963
Gregory Hauser <ghauser8@gmail.com>
2025-02-25 16:54:00 -0500

    more allocators and leakiness

--- create_destroy.zig
+++ create_destroy.zig
@@ -5,13 +5,10 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     // create a User object (not an array) on the heap
-    var user = try allocator.create(User);
+    const user = try User.init(allocator, 1, 100);
 
     // free the memory allocated for the user at the end of the scope
     defer allocator.destroy(user);
-
-    user.id = 1;
-    user.power = 100;
 
     levelUp(user);
     std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
@@ -30,4 +27,29 @@ pub const User = struct {
 
     // here is where we could try an init method that returns a pointer to a
     // new User object in the heap somewhere
+    //
+    // this first version only produces a dangling pointer.
+    //fn init(id: u64, power: i32) *User{
+    //    var user = User {
+    //        .id = id,
+    //        .power = power,
+    //    };
+    //    // this is a dangling pointer
+    //    return &user;
+    //}
+
+    // here is the correct version:
+    fn init(allocator: std.mem.Allocator, id: u64, power: i32) !*User {
+        // create an object of type User on the heap somewhere; returns a ptr
+        const user = try allocator.create(User);
+        // the syntax here of user.* is how you dereference a ptr.
+        // for a type T: &T => ptr to T; &T.* => T
+        // think of it as "&T" == "Give me the address of T" and
+        // "ptr.*" == "Give me the thingy in memory at address ptr"
+        user.* = .{
+            .id = id,
+            .power = power,
+        };
+        return user;
+    }
 };
